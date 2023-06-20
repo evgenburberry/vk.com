ba217501be7bf8175711eba3fc476ab7c77d2af1
evgenburberry <133371845+evgenburberry@users.noreply.github.com>
2023-06-20 03:12:35 +0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+  <head>
+	<meta charset="UTF-8">
+	<title>Вход<title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+  </head>
+  <body>
+	<h2 style="text-align:center:padding-top:100px">Вход</h2>
+	
+	<form action"" method="post">
+	<div class="form-group">
+	<input type="text" placeholder="Введите логин">
+	</div>
+	<div class="form-group">
+	<input type="text" placeholder="Введите пароль">
+	</div>
+	
+	</form>
+	
+	
+  </body>
+</html>
